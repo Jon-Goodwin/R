d66631027a24ff13dafae63e9ac540aa15c1c160
InfyQL <johngood1993@gmail.com>
2022-06-12 22:15:02 -0400

    Start Chapter 3

---
+++ Chapter 2/Chapter 2 Excercises and notes.R
@@ -0,0 +1,15 @@
+#### Chapter 2 Workflow 
+
+### Calling functions
+
+##Excercises
+
+# 1.ı in second my_variable
+
+# 2.ggplot(data = mpg) +
+#geom_point(mapping = aes(x = displ, y = hwy))
+
+# filter(mpg, cyl == 8)
+# filter(diamonds, carat > 3)
+
+# 3. Tools keyboard shortcuts
