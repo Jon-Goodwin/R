bf0da56f2ea1c66b190688baa4a15144c47b283d
Jon Goodwin <johngood1993@gmail.com>
2023-02-05 21:47:56 -0500

    Chapter 21 complete

--- Part 5/Part 5 Communicate.R
+++ Part 5/Part 5 Communicate.R
@@ -13,3 +13,26 @@
 # Outputs are different file types, the input change is what the YAML header
 # contains being output: pdf_document to html_document, word_document.
 
+# cached = TRUE will cache the output of a code chunk to avoid rerunning
+
+# dependson = "name" will not an onbject the code chunk depends on and will
+# update cache if that object changes.
+
+# knitr::clean_cache() to clear cache
+
+### Excercises 
+
+# 1.
+diamonds %>%
+  mutate(volume = x*y*z) %>%
+  filter(volume < 1000) %>%
+  ggplot(aes(x = as.factor(cut), y = volume))+geom_boxplot()
+
+# 3.
+
+diamonds %>%
+  mutate(volume = x*y*z) %>%
+  arrange(-volume) %>%
+  slice(1:20)
+
+comma <- function(x) format(x, digits = 2, big.mark = ",")
